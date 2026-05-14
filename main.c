4fddebfa38d08e75b47b15c9a95813a8971f9078
MJSafwan <MJsafwaan@gmail.com>
2026-05-14 22:24:26 +0300

    Better routines

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
 
-#define ROUTINE_ONE_DONE 1
-#define ROUTINE_ALL_DONE 2
-
-void routine(void *fn, void *arg);
+/* Implemented in routine.asm */
+uint64_t routine(void *stacks, void *fn, void *arg);
 void routine_yield(void);
-int routine_next(void);
+int routine_run(uint64_t routine);
 void routine_finish(void);
 
 void fun(void *arg) {
@@ -25,15 +25,57 @@ void quick(void *arg) {
     routine_finish();
 }
 
+#define ROUTINE_YIELD 0
+#define ROUTINE_DONE 1
+#define ROUTINE_CAPACITY 256
+#define STACK_SIZE 4096
+
+typedef uint64_t routine_t;
+typedef void* rctx_t;
+
+typedef struct {
+    routine_t items[ROUTINE_CAPACITY];
+    size_t count;
+} routine_list;
+
+rctx_t routine_init(size_t ns) {
+    if (ns == 0)
+        return NULL;
+    return calloc(ns, STACK_SIZE);
+}
+
+void routine_append(routine_list *s, routine_t r) {
+    if (s->count == ROUTINE_CAPACITY)
+        return;
+    s->items[s->count++] = r;
+}
+
+void routine_remove(routine_list *s, routine_t r) {
+    size_t i = 0;
+    for (i = 0; s->items[i] != r; ++i);
+    
+    for (size_t j = i; j < s->count-1; ++j) {
+        routine_t tmp = s->items[j];
+        s->items[j] = s->items[j+1];
+        s->items[j+1] = tmp;
+    }
+
+    s->count--;
+}
+
 int main(void) {
-    routine(fun, "foo");
-    routine(quick, NULL);
-    routine(fun, "bar");
-    routine(fun, "baz");
-    while (routine_next() != ROUTINE_ONE_DONE);
-    printf("'quick' probably finished, running through the others!\n");
-    routine(fun, "fun");
-    while (routine_next() != ROUTINE_ALL_DONE);
-    printf("All done!\n");
+    routine_list s = {0};
+    rctx_t context = routine_init(ROUTINE_CAPACITY);
+    routine_append(&s, routine(context, fun, "foo"));
+    routine_append(&s, routine(context, quick, NULL));
+    routine_append(&s, routine(context, fun, "bar"));
+    routine_append(&s, routine(context, fun, "baz"));
+    while (s.count != 0) {
+        for (size_t i = 0; i < s.count; ++i) {
+            if (routine_run(s.items[i]) == ROUTINE_DONE) {
+                routine_remove(&s, s.items[i]);
+            }
+        }
+    }
     return 0;
 }
