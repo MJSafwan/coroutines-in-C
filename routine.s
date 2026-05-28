3505fc00f7361255f63fcf4962099bf96e054398
MJSafwan <MJsafwaan@gmail.com>
2026-05-28 21:54:28 +0300

    Saved the scheduler's registers

--- routine.s
+++ routine.s
@@ -1,4 +1,8 @@
 .align 4
+
+.equ STACK_PRE_SIZE, 240
+.equ STACK_OFF, STACK_PRE_SIZE+16
+
 .section __TEXT,__text
 .global _routine
 ; uint64_t routine(void *ctx, void *fn, void *arg)
@@ -21,7 +25,7 @@ routine_mk_body:
 routine_mk_end:
     mov x6, x11
     mov x10, x11
-    sub x10, x10, #240
+    sub x10, x10, STACK_PRE_SIZE
     stp x10, x1, [x11, #-16]!
     sub x11, x11, #112
     stp x2, xzr, [x11, #-16]! 
@@ -32,11 +36,14 @@ routine_mk_end:
 ; void routine_yield(void)
 _routine_yield:
     mov x11, x29
-    add x11, x11, #256
+    add x11, x11, STACK_OFF
 
     mov x12, sp
     ; shamelessly stolen from musl's setjmp implementation (with modification)
     stp x12, x30, [x11, #-16]!
+    mov x13, x11
+
+
     str x29, [x11, #-8]!
     sub x11, x11, #8
     ldp x29, x30, [x11, #-16]!
@@ -50,7 +57,19 @@ _routine_yield:
     stp d10, d11, [x11, #-16]!
     stp d12, d13, [x11, #-16]!
     stp d14, d15, [x11, #-16]!
+
     ldr x12, [x11, #-16]! 
+
+    ldp x19, x20, [x11, #-16]!
+    ldp x21, x22, [x11, #-16]!
+    ldp x23, x24, [x11, #-16]!
+    ldp x25, x26, [x11, #-16]!
+    ldp x27, x28, [x11, #-16]!
+    ldp x0, x1, [x11, #-16]!
+    ldp d8, d9, [x11, #-16]!
+    ldp d10, d11, [x11, #-16]!
+    ldp d12, d13, [x11, #-16]!
+    ldp d14, d15, [x11, #-16]!
 
     mov sp, x12
     mov w0, #0
@@ -60,7 +79,7 @@ _routine_yield:
 ; void routine_sleep(uint64_t ms)
 _routine_sleep:
     mov x11, x29
-    add x11, x11, #256
+    add x11, x11, STACK_OFF
 
     mov x12, sp
     stp x12, x30, [x11, #-16]!
@@ -79,6 +98,18 @@ _routine_sleep:
     stp d14, d15, [x11, #-16]!
     ldr x12, [x11, #-16]! 
 
+
+    ldp x19, x20, [x11, #-16]!
+    ldp x21, x22, [x11, #-16]!
+    ldp x23, x24, [x11, #-16]!
+    ldp x25, x26, [x11, #-16]!
+    ldp x27, x28, [x11, #-16]!
+    ldp x0, x1, [x11, #-16]!
+    ldp d8, d9, [x11, #-16]!
+    ldp d10, d11, [x11, #-16]!
+    ldp d12, d13, [x11, #-16]!
+    ldp d14, d15, [x11, #-16]!
+
     mov sp, x12
     cmp x2, 0
     b.eq routine_sleep_ret
@@ -92,7 +123,7 @@ routine_sleep_ret:
 ; void routine_panic(uint64_t panic_code)
 _routine_panic:
     mov x11, x29
-    add x11, x11, #256
+    add x11, x11, STACK_OFF
     sub x11, x11, #24
     ldr x2, [x11, #-8]!
     ldp x29, x30, [x11, #-16]!
@@ -110,7 +141,7 @@ routine_panic_ret:
 ; void routine_await(routine_t a_routine)
 _routine_await:
     mov x11, x29
-    add x11, x11, #256
+    add x11, x11, STACK_OFF
 
     mov x12, sp
     stp x12, x30, [x11, #-16]!
@@ -129,6 +160,18 @@ _routine_await:
     stp d14, d15, [x11, #-16]!
     ldr x12, [x11, #-16]! 
 
+
+    ldp x19, x20, [x11, #-16]!
+    ldp x21, x22, [x11, #-16]!
+    ldp x23, x24, [x11, #-16]!
+    ldp x25, x26, [x11, #-16]!
+    ldp x27, x28, [x11, #-16]!
+    ldp x0, x1, [x11, #-16]!
+    ldp d8, d9, [x11, #-16]!
+    ldp d10, d11, [x11, #-16]!
+    ldp d12, d13, [x11, #-16]!
+    ldp d14, d15, [x11, #-16]!
+
     mov sp, x12
     cmp x2, 0
     b.eq routine_await_ret
@@ -144,6 +187,19 @@ _routine_run:
     ldp x13, x10, [x11, #-16]!
     cmp x10, #0
     b.eq routine_next_finish
+
+    mov x12, x11
+    sub x12, x12, #208
+    stp x19, x20, [x12, #-16]!
+    stp x21, x22, [x12, #-16]!
+    stp x23, x24, [x12, #-16]!
+    stp x25, x26, [x12, #-16]!
+    stp x27, x28, [x12, #-16]!
+    stp x0, x1, [x12, #-16]!
+    stp d8, d9, [x12, #-16]!
+    stp d10, d11, [x12, #-16]!
+    stp d12, d13, [x12, #-16]!
+    stp d14, d15, [x12, #-16]!
     
     mov x14, x29
     ldr x29, [x11, #-8]!
@@ -159,10 +215,10 @@ _routine_run:
     ldp d10, d11, [x11, #-16]!
     ldp d12, d13, [x11, #-16]!
     ldp d14, d15, [x11, #-16]!
-
     mov x12, sp
     str x12, [x11, #-16]! 
     mov sp, x13
+
     br x10
 routine_next_finish:
     ret
@@ -171,7 +227,7 @@ routine_next_finish:
 ; void routine_finish(void)
 _routine_finish:
     mov x11, x29
-    add x11, x11, #256
+    add x11, x11, STACK_OFF
     stp xzr, xzr, [x11, #-16]!
     sub x11, x11, #16
     ldp x29, x30, [x11, #-16]!
